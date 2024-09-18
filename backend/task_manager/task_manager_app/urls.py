db3bdce11e45da3e46821a8925f046b18684e73d
eskimosa <esskimosa@gmail.com>
2024-09-18 20:08:37 +0200

    worked on autofill functionality

--- backend/task_manager/task_manager_app/urls.py
+++ backend/task_manager/task_manager_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import TaskViewSet
+from .views import TaskViewSet, TagAutoFillView
 
 urlpatterns = [
     path('list_tasks/', TaskViewSet.as_view({'get': 'list'}), name='tasks'),
     path('get_task/<pk>/', TaskViewSet.as_view({'get': 'retrieve'}), name='retrieve_task'),
     path('add_task/', TaskViewSet.as_view({'post': 'create'}), name='create_task'),
+    path('tags_autofill/', TagAutoFillView.as_view({'get': 'list'}), name='tag_autofill'),
     # path('auth/', include('task_manager_app.authentication.urls')),
 ]
